4e7439af3299f19427a027a1d2f2ac93a49501d5
Anton Povarov <anton.povarov@gmail.com>
2014-02-25 20:55:26 +0400

    removed dead code

--- php_pinba.h
+++ php_pinba.h
@@ -59,9 +59,6 @@ ZEND_BEGIN_MODULE_GLOBALS(pinba) /* {{{ */
 	pinba_collector collectors[PINBA_COLLECTORS_MAX];
 	unsigned int n_collectors; /* number of collectors we got from ini file */
 	char *collector_address; /* this is a lil broken, contains last address only */
-	/* FIXME: remove these next two */
-//	char *server_host;
-//	char *server_port;
 #if PHP_VERSION_ID < 50400
 	int (*old_sapi_ub_write) (const char *, unsigned int TSRMLS_DC);
 #endif
